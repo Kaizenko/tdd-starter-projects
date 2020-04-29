533bb94b08630ea3d7993ee470c10417f2ea6f23
Fadi Stephan <fstephan@users.noreply.github.com>
2020-04-28 21:11:16 -0400

    Initial Commit

---
+++ VendingMachineExercise/src/test/java/com/kaizenko/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,16 @@
+package com.kaizenko.vendingmachine;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class VendingMachineTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
